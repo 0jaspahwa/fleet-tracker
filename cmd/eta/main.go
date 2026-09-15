36094f4a4285c993b32e811d9b83df4f5d8f0ec2
0jaspahwa <ojaspahwa20@gmail.com>
2026-09-15 16:22:37 +0530

    feat(eta): build rolling per-driver ping window

---
+++ cmd/eta/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	//"encoding/json"
+	"log"
+	//"math"
+	"os"
+	"os/signal"
+	//"time"
+
+	fleetv1 "fleettracker/gen/fleet/v1"
+
+	"github.com/segmentio/kafka-go"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type Point struct {
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	TimestampMs int64 `json:"ts_ms"`
+}
+// we need something to hold the history of driver(pings) because if use map -> unordered sequence of the driver
+// we need ordered sequence -> using SLICE -> kinda same like a vector
+
+// window size for slicing
+const windowSize = 5
+
+
+func main(){
+	//ctx -> letting the fucn know when to stop
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	
+	//read the driver pings form the topic location pings
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"127.0.0.1:9092"},
+		Topic: "location.pings",
+		GroupID: "eta",
+		StartOffset: kafka.FirstOffset,
+		ErrorLogger: kafka.LoggerFunc(log.Printf),
+	})
+	defer reader.Close()
+
+	// we need to store the driver and its history
+	m := make(map[string][]Point)	
+	
+	
+	for{
+		//read the message from the ping
+		msg, err := reader.FetchMessage(ctx)
+		if err != nil {
+			log.Printf("stopping: %v", err)
+			return
+		}
+		var ping fleetv1.StreamLocationRequest
+		
+		if err := protojson.Unmarshal(msg.Value, &ping); err != nil {
+			log.Printf("bad message p=%d offset=%d: %v", msg.Partition, msg.Offset, err)
+			continue
+		}
+		driverId := ping.GetDriverId()
+		latitude := ping.GetLatitude()
+		longitude := ping.GetLongitude()
+
+
+		pt := Point{
+			Latitude:    latitude,
+			Longitude:   longitude,
+			TimestampMs: ping.GetTimestampMs(),
+		}
+
+		//storing in the map
+		m[driverId] = append(m[driverId], pt)
+		
+		//slicing the window to the N size cap
+		if len(m[driverId]) > windowSize{
+			m[driverId] = m[driverId][1:] //takes the slice from index 1 to the end
+		}
+		log.Printf("%s has %d points", driverId, len(m[driverId]))
+
+		if err := reader.CommitMessages(ctx, msg); err != nil {
+			log.Printf("commit failed: %v", err)
+		}
+	}
+	
+
+
+
+}
